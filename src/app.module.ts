acbc2fd83301effdf8f37465e2d91efdadb0f64b
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2023-12-13 23:03:47 +0400

    feat: create user_db database with User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5432),
+        username: configService.get<string>('DATABASE_USER', 'your_username'),
+        password: configService.get<string>(
+          'DATABASE_PASSWORD',
+          'your_password',
+        ),
+        database: configService.get<string>('DATABASE_NAME', 'your_database'),
+        entities: [User],
+        synchronize: configService.get<boolean>('TYPEORM_SYNC', false), // Use false for the production!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
