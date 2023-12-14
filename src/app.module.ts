ff087fd3f7ccc3539f46768b920b827757826d54
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2023-12-14 17:51:10 +0400

    feat: add Prisma
    also remove typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './entities/user.entity';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST', 'localhost'),
-        port: configService.get<number>('DATABASE_PORT', 5432),
-        username: configService.get<string>('DATABASE_USER', 'your_username'),
-        password: configService.get<string>(
-          'DATABASE_PASSWORD',
-          'your_password',
-        ),
-        database: configService.get<string>('DATABASE_NAME', 'your_database'),
-        entities: [User],
-        synchronize: configService.get<boolean>('TYPEORM_SYNC', false), // Use false for the production!
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/enums/language.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Language {
-  English = 'English',
-  Russian = 'Russian',
-}
